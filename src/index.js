ef11b29651396179644c5287e3625022b2193371
Ryan Wilson <syndicated.life@gmail.com>
2017-02-10 21:30:09 -0800

    use https for docs

--- src/index.js
+++ src/index.js
@@ -116,7 +116,8 @@ module.exports = function (startServer = true) {
       info: {
         title: 'XIVLOGS',
         version: Pack.version
-      }
+      },
+      schemes: ['https']
     }
   }, {
     register: Limiter,
